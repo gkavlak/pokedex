05f1a6bd493a4d640487fbe9b5a1c1ac388558db
gkavlak <63876977+gkavlak@users.noreply.github.com>
2024-05-02 23:08:51 +0300

    commit test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>heelo world</Text>
+      <Text>heelo gogogo</Text>
       <StatusBar style="auto" />
     </View>
   );
