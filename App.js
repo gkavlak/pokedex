fbc588998f25c632543a52f55e6c1838c1e33e17
gkavlak <63876977+gkavlak@users.noreply.github.com>
2024-05-02 23:05:05 +0300

    inital commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start sadad on your app!</Text>
+      <Text>heelo world</Text>
       <StatusBar style="auto" />
     </View>
   );
