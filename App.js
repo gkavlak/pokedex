5050ae449493fcfede1e83e2d16e5c7dcef4e4ff
gkavlak <63876977+gkavlak@users.noreply.github.com>
2024-05-03 13:26:13 +0300

    added pikachu

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+ const BASE_URL ="https://my-json-server.typicode.com/gkavlak/pokedex";
 
 export default function App() {
+  const [pokemons, setPokemons] = useState([])
+  useEffect(() => {
+    fetch('${BASE_URL}/pokemons')
+    .then(res => res.json())
+    .then(data =>setPokemons(data));
+
+  }, []);
+  
   return (
-    <View style={styles.container}>
-      <Text>heelo gogogo</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+      data={pokemons}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({item}) => <Text>{item.name} </Text>}
+      />
+
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
